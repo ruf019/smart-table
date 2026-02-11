b121d4c3c9107f73c370b0191308035715628033
Ruslan Fedorov <rofedorov@edu.hse.ru>
2026-02-12 00:52:21 +0300

    Выведены данные в таблицу

--- src/components/table.js
+++ src/components/table.js
@@ -10,14 +10,45 @@ import {cloneTemplate} from "../lib/utils.js";
 export function initTable(settings, onAction) {
     const {tableTemplate, rowTemplate, before, after} = settings;
     const root = cloneTemplate(tableTemplate);
-
+    
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    before.reverse().forEach(subName => {                  // перебираем нужный массив идентификаторов
+        root[subName] = cloneTemplate(subName);            // клонируем и получаем объект, сохраняем в таблице
+        root.container.prepend(root[subName].container);   // добавляем к таблице до (prepend)
+    });
+    
+    after.forEach(subName => {
+        root[subName] = cloneTemplate(subName);
+        root.container.append(root[subName].container);    // добавляем к таблице после (append)
+    });
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
+    root.container.addEventListener('change', () => {
+        onAction();
+    })
+
+    root.container.addEventListener('reset', () => {
+        setTimeout(onAction());
+    })
+
+    root.container.addEventListener('submit', (e) => {
+        e.preventDefault();
+        onAction(e.submitter);
+    })
 
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate);
+            
+            Object.keys(item).forEach(key => {
+                if (row.elements[key]) {
+                    row.elements[key].textContent = item[key];
+                }
+            })
+            
+            return row.container
+        });
         root.elements.rows.replaceChildren(...nextRows);
     }
 
